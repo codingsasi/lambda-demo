3dea1da6fbdf628447fceab6a8e1ec3aaf919d25
Abhai Sasidharan <abhai@qburst.com>
2019-07-28 03:38:08 +0530

    Demo lambda function that decides the redirect url based on user location

---
+++ lambda_function.py
@@ -0,0 +1,40 @@
+import json
+import os
+import ipaddress
+
+def lambda_handler(event, context):
+    #Logic goes here. The below code is just a dummy.
+    if (validate_ipaddress(event['queryStringParameters']['ip']) == False):
+        return {
+            'statusCode': 200,
+            'body': json.dumps("https://www.abhaisasidharan.xyz")
+        }
+    result = os.popen("curl https://ipinfo.io/" + event['queryStringParameters']['ip']).read()
+    sites = dict([('IN', 'react.abhaisasidharan.xyz'), ('US', 'ng.abhaisasidharan.xyz'), ('any', 'vue.abhaisasidharan.xyz')])
+    result_json = json.loads(result)
+    
+    if (result_json['country'] == 'IN'):
+        redirect_to = 'https://react.abhaisasidharan.xyz'
+    elif (result_json['country'] == 'US'):
+        redirect_to = 'https://ng.abhaisasidharan.xyz'
+    else:
+        redirect_to = 'https://vue.abhaisasidharan.xyz'
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(redirect_to)
+    }
+    
+def validate_ipaddress(ip):
+    try:
+        parts = ip.split(".")
+        if len(parts) != 4:
+            return False
+        for item in parts:
+            if not 0 <= int(item) <= 255:
+                return False
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError as errorCode:
+        pass
+        return False
